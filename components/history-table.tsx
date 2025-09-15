47efb36bfbf78405b250df53138e460c2f06039b
Santiago Torres <105675754+santy500@users.noreply.github.com>
2025-09-15 11:33:07 -0300

    Update history-table.tsx
    cambio de case obesidad a obeso

--- components/history-table.tsx
+++ components/history-table.tsx
@@ -13,7 +13,7 @@ const getCategoryBadge = (categoria: string) => {
       return "bg-green-100 text-green-800 border-green-200"
     case "sobrepeso":
       return "bg-amber-100 text-amber-800 border-amber-200"
-    case "obesidad":
+    case "obeso":
       return "bg-red-100 text-red-800 border-red-200"
     default:
       return "bg-gray-100 text-gray-800 border-gray-200"
